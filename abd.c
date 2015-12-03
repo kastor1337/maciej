2303561b8b3e2c320fe011a1b2774a3f5a43df83
nhattrinh <minhnhattrinh1995@gmail.com>
2015-12-03 09:24:05 +0100

    dsad

--- abd.c
+++ abd.c
@@ -6,7 +6,7 @@ int main()
 {
   int m, n, p, q, c, d, k, sum = 0;
   int first[10][10], second[10][10], multiply[10][10];
- 
+  printf("npg");
   printf("Enter the number of rows and columns of first matrix\n");
   scanf("%d%d", &m, &n);
   printf("Enter the elements of first matrix\n");
